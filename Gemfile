de15685784c14ea6cf0d727dcbf51b629cdbfd32
lukasz.pelka <lukpel32@gmail.com>
2014-10-05 17:33:38 -0700

    Add Amazon S3 for Paperclip uploads to Heroku

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'devise', '~> 3.3.0.rc2'
 gem 'paperclip', github: 'thoughtbot/paperclip'
+gem 'aws-sdk', '~> 1.54.0'
 group :development, :test do
      gem 'sqlite3'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,13 @@ Rails.application.configure do
   #required for heroku
   config.action_mailer.default_url_options = { host: 'travelsharetime.com' }
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
+
 end
