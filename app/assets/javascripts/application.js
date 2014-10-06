f9917ef1a9cb6c1d30a83f783478277e71a96205
lukasz.pelka <lukpel32@gmail.com>
2014-10-05 19:56:42 -0700

    chaged js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require masonry/jquery.masonry
 //= require turbolinks
+//= require masonry/jquery.masonry
 //= require_tree .
