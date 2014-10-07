41f1c74504c134041cdb3d8b3bfc15b5e36dea0e
lukasz.pelka <lukpel32@gmail.com>
2014-10-06 18:53:37 -0700

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,13 +27,13 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
-  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
+
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,6 @@
 Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
-config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
+
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
