286bd76d47903164d40b094ad0400fffa2066d06
lukasz.pelka <lukpel32@gmail.com>
2014-10-12 20:06:57 -0700

    delete other users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, [:edit, :update, :destroy]
   def index
    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
  end
@@ -44,7 +44,10 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
