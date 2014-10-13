8a48f517a30fdf7f36fd258ef308ec04663c4633
lukasz.pelka <lukpel32@gmail.com>
2014-10-12 20:49:40 -0700

    add names to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,10 @@ class Pin < ActiveRecord::Base
             :bucket => 'travelsharetime',
             :s3_credentials => {:access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
 validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+validates :image, presence: true
+  validates :description, presence: true
+
 end
+
+
