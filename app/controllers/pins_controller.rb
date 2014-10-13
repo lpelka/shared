0748daf45a2429709ea695c7805c6585831475d5
lukasz.pelka <lukpel32@gmail.com>
2014-10-12 20:22:41 -0700

    destroy

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update, :destroy]
+  
   before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
   def index
    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
